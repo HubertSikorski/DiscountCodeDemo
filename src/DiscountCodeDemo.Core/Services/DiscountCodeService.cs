04985daa6a21cd02233664df8240471d3d87c0fc
hubert <huberrt.sikorski@gmail.com>
2025-10-15 13:49:56 +0200

    Add logic for discount code generation and unit tests

--- src/DiscountCodeDemo.Core/Services/DiscountCodeService.cs
+++ src/DiscountCodeDemo.Core/Services/DiscountCodeService.cs
@@ -5,8 +5,40 @@ namespace DiscountCodeDemo.Core.Services;
 
 public class DiscountCodeService : IDiscountCodeService
 {
-    public Task<GenerateResponse> GenerateDiscountCodesAsync(ushort count, byte length)
+    private readonly HashSet<string> _discountCodes = new HashSet<string>();
+    public async Task<GenerateResponse> GenerateDiscountCodesAsync(ushort count, byte length)
     {
-        throw new NotImplementedException();
+        var response = new GenerateResponse()
+        {
+            Result = true,
+            Codes = new List<string>()
+        };
+
+        for (int i = 0; i < count; i++)
+        {
+            string code;
+
+            do
+            {
+                code = GenerateRandomCode(length);
+            } while (_discountCodes.Contains(code));
+            
+            _discountCodes.Add(code);
+            response.Codes.Add(code);
+        }
+
+        return await Task.FromResult(response);
+    }
+
+    private string GenerateRandomCode(byte length)
+    {
+        const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        
+        var random = new Random();
+        var code = new string(Enumerable.Range(0, length)
+            .Select(c => validChars[random.Next(validChars.Length)])
+            .ToArray());
+
+        return code;
     }
 }
---
+++ tests/DiscountCodeDemo.UnitTests/DiscountCodeServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using DiscountCodeDemo.Core.Services;
+
+namespace DiscountCodeDemo.UnitTests;
+
+public class DiscountCodeServiceTests
+{
+    private readonly DiscountCodeService _service;
+
+    public DiscountCodeServiceTests()
+    {
+            _service = new DiscountCodeService();
+    }
+
+    [Fact]
+    public async Task GenerateDiscountCodeAsync_ShouldGenerateUniqueDiscountCodes()
+    {
+        //Act
+        var result = await _service.GenerateDiscountCodesAsync(10, 8);
+        
+        //Arrange
+        Assert.True(result.Result);
+        Assert.Equal(10, result.Codes.Count);
+        Assert.Equal(result.Codes.Distinct().Count(), result.Codes.Count);
+    }
+}
