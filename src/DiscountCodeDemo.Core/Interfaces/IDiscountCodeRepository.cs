b201d50d883a4eb494eb54294025c397e8e0054c
hubert <huberrt.sikorski@gmail.com>
2025-10-15 14:58:17 +0200

    Add interface for discount codes repository

---
+++ src/DiscountCodeDemo.Core/Interfaces/IDiscountCodeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DiscountCodeDemo.Core.Models;
+
+namespace DiscountCodeDemo.Core.Interfaces;
+
+public interface IDiscountCodeRepository
+{
+    Task<IEnumerable<DiscountCodeEntity>> GetALlAsync();
+    Task<bool> ExistsAsync(string code);
+    Task AddManyAsync(IEnumerable<DiscountCodeEntity> codeList);
+    Task<DiscountCodeEntity?> GetByCodeAsync(string code);
+    Task<bool> MarkCodeAsUsedAsync(string code);
+    Task SaveChangesAsync();
+}
---
+++ src/DiscountCodeDemo.Core/Models/DiscountCodeEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace DiscountCodeDemo.Core.Models;
+
+public class DiscountCodeEntity
+{
+    public string Code { get; set; } = string.Empty;
+    public bool IsUsed { get; set; } = false;
+}
