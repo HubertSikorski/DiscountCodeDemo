66b27a1531d8ab28b485f02cd0dbfb2f0d5e754d
hubert <huberrt.sikorski@gmail.com>
2025-10-15 14:06:30 +0200

    Add more unit tests for code generation

--- tests/DiscountCodeDemo.UnitTests/DiscountCodeServiceTests.cs
+++ tests/DiscountCodeDemo.UnitTests/DiscountCodeServiceTests.cs
@@ -22,4 +22,41 @@ public class DiscountCodeServiceTests
         Assert.Equal(10, result.Codes.Count);
         Assert.Equal(result.Codes.Distinct().Count(), result.Codes.Count);
     }
+
+    [Fact]
+    public async Task GenerateDiscountCodeAsync_ShouldHandleLargeNumberOfCodes()
+    {
+        //Arrange
+        ushort count = 2000;
+        
+        //Act
+        var result = await _service.GenerateDiscountCodesAsync(count, 8);
+        
+        //Assert
+        Assert.Equal(count, result.Codes.Count);
+    }
+
+    [Fact]
+    public async Task GenerateDiscountCodeAsync_ShouldReturnEmptyList_WhenCountIsZero()
+    {
+        //Act
+        var result = await _service.GenerateDiscountCodesAsync(0, 8);
+        
+        //Assert
+        Assert.True(result.Result);
+        Assert.Empty(result.Codes);
+    }
+
+    [Fact]
+    public async Task GenerateDiscountCodeAsync_ShouldGenerateCodesWithCorrectLength()
+    {
+        //Arange
+        byte codeLength = 7;
+        
+        //Act
+        var result = await _service.GenerateDiscountCodesAsync(10, codeLength);
+        
+        //Assert
+        Assert.All(result.Codes, code => Assert.Equal(codeLength, code.Length));
+    }
 }
