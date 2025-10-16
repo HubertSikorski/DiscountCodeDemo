3f5e0f4fb285868d4e00d9ed271924bdfb9f6171
hubert <huberrt.sikorski@gmail.com>
2025-10-16 19:09:43 +0200

    Add MessageReader and MessageWriter for custom TCP protocol

---
+++ src/DiscountCodeDemo.Server/Protocol/MessageReader.cs
@@ -0,0 +1,48 @@
+﻿using System.Buffers.Binary;
+using System.Net.Sockets;
+using DiscountCodeDemo.Server.Protocol.Messages;
+
+namespace DiscountCodeDemo.Server.Protocol;
+
+public class MessageReader
+{
+    private readonly NetworkStream _networkStream;
+
+    public MessageReader(NetworkStream networkStream)
+    {
+        _networkStream = networkStream;
+    }
+
+    public async Task<(RequestType Type, byte[] Data)> ReadMessageAsync()
+    {
+        var header = new byte[3];
+        int bytesRead = 0;
+
+        while (bytesRead < header.Length)
+        {
+            int read = await _networkStream.ReadAsync(header, bytesRead, header.Length - bytesRead);
+
+            if (read == 0)
+                throw new IOException("[Server] Connection closed while reading header");
+
+            bytesRead += read;
+        }
+
+        var type = (RequestType)header[0];
+        ushort payloadLength = BinaryPrimitives.ReadUInt16BigEndian(header.AsSpan(1));
+
+        var payload = new byte[payloadLength];
+        bytesRead = 0;
+
+        while (bytesRead < payloadLength)
+        {
+            int read = await _networkStream.ReadAsync(payload, bytesRead, payloadLength - bytesRead);
+            if (read == 0)
+                throw new IOException("[Server] Connection closed while reading payload");
+
+            bytesRead += read;
+        }
+
+        return (type, payload);
+    }
+}
---
+++ src/DiscountCodeDemo.Server/Protocol/MessageWriter.cs
@@ -0,0 +1,38 @@
+﻿using System.Buffers.Binary;
+using System.Net.Sockets;
+using System.Text;
+using DiscountCodeDemo.Server.Protocol.Messages;
+
+namespace DiscountCodeDemo.Server.Protocol;
+
+public class MessageWriter
+{
+    private readonly NetworkStream _networkStream;
+
+    public MessageWriter(NetworkStream networkStream)
+    {
+        _networkStream = networkStream;
+    }
+
+    public async Task SendMessageAsync(RequestType type, byte[] payload)
+    {
+        var header = new byte[3];
+        header[0] = (byte)type;
+        BinaryPrimitives.WriteInt32BigEndian(header.AsSpan(1), (ushort)payload.Length);
+        
+        await _networkStream.WriteAsync(header);
+        await _networkStream.WriteAsync(payload);
+    }
+
+    public async Task SendBooleanResponseAsync(RequestType type, bool success)
+    {
+        byte[] payload = new[] {success ? (byte)1 : (byte)0};
+        await SendMessageAsync(type, payload);
+    }
+
+    public async Task SendErrorAsync(string errorMessage)
+    {
+        byte[] payload = Encoding.UTF8.GetBytes(errorMessage);
+        await SendMessageAsync(RequestType.Error, payload);
+    }
+}
