b1a3ece74f586c4d468198839132195bf808107a
hubert <huberrt.sikorski@gmail.com>
2025-10-16 18:27:21 +0200

    Add request type enum and constants for validation

---
+++ src/DiscountCodeDemo.Server/Protocol/Messages/RequestType.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscountCodeDemo.Server.Protocol.Messages;
+
+public enum RequestType : byte
+{
+    Generate = 0x01,
+    Use = 0x02,
+    Status = 0x03,
+    Error = 0xFF
+}
---
+++ src/DiscountCodeDemo.Server/Protocol/ProtocolConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscountCodeDemo.Server.Protocol;
+
+public class ProtocolConstants
+{
+    public const byte MinCodeLength = 7;
+    public const byte MaxCodeLength = 8;
+    public const ushort MaxCodeCount = 2000;
+}
